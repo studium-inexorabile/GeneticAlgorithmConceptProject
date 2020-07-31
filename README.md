6299ce235b8081aed8228102dd68aed6d42a28f7
studium-inexorabile <68911602+studium-inexorabile@users.noreply.github.com>
2020-07-31 11:31:02 -0500

    added test evaluation to testCreation.js

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # TestScripts
 Scripts for creation and evaluation of mock test.
 
-To create a test, run node testCreation.js (test is stored in test.json)
+To create a test, run node testCreation.js (test is stored in test.json, max/min test score and evalutation is shown in terminal window)
+
 To take a test, run node testTaking.js (taken test is stored in takenTest.json)
+
 To score test, run node testScoring.js (score is shown in terminal window)
+
+
 
 WEIGHTS
 weights for answers can be modified on line 25 of testCreation.js
--- testCreation.js
+++ testCreation.js
@@ -39,6 +39,27 @@ const questionCreator = () => {
 }
 questionCreator();
 
+const evaluateTest = () => {
+    let min = 0;
+    let max = 0;
+    for(i in test){
+        let weights = test[i]["answers"].map(item => item.weight)
+        min += parseFloat(Math.min(...weights))
+        max += parseFloat(Math.max(...weights))
+    }
+    console.log("Maximum test score: ", max)
+    console.log("Minimum test score: ", min)
+    console.log()
+    let breakpoints = ((max - min) / 5)
+    console.log("Potential breakpoints")
+    console.log("Low - <", (breakpoints * 1  + min)  + .1)
+    console.log("Medium-Low - <", (breakpoints * 2  + min)  + .1)
+    console.log("Medium - <", (breakpoints * 3  + min)  + .1)
+    console.log("Medium-High - <", (breakpoints * 4  + min)  + .1)
+    console.log("High - <", (breakpoints * 5  + min)  + .1)
+}
+evaluateTest()
+
 const json = JSON.stringify(test)
 fs.writeFileSync('./test.json', json, (err) => {
     if(err){
