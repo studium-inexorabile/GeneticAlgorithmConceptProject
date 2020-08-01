ce0a7a06098ef33630a6e59c8628ffd0b0e5f68c
studium-inexorabile <68911602+studium-inexorabile@users.noreply.github.com>
2020-07-31 23:05:53 -0500

    added improved functionality to tests and added GA file

--- testTaking.js
+++ testTaking.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const teskTaking = () => {
+const teskTaking = (answers = []) => {
     //dictionary to use for converting between array position and answer choice
     let multipleChoice = {
         0: 'A',
@@ -9,11 +9,18 @@ const teskTaking = () => {
         3: 'D',
         4: 'E'
     }
+    //uses given answer array or
     //fills object with random answer choices (A-E)
     let takenTest = {}
     for(let i = 1; i <= 10; i++){
-        takenTest[i] = {
-            chosenAnswer : multipleChoice[Math.floor(Math.random() * 5)]
+        if(answers.length > 0){
+            takenTest[i] = {
+                chosenAnswer : answers[i - 1]
+            }
+        }else {
+            takenTest[i] = {
+                chosenAnswer : multipleChoice[Math.floor(Math.random() * 5)]
+            }
         }
     }
     //writes answers to takenTest.json
